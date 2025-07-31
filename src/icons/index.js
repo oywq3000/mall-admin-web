cd426ae13f7e3e7d5b4a778e04b6a75bd572b8a2
oywq2020 <2403_87019412@noreply.gitcode.com>
2025-07-31 19:04:19 +0800

    complete the link between front and back

--- src/icons/index.js
+++ src/icons/index.js
@@ -6,4 +6,5 @@ Vue.component('svg-icon', SvgIcon)
 
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 const req = require.context('./svg', false, /\.svg$/)
+
 requireAll(req)
